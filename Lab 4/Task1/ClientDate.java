8f40c4483bef40244f6344c9f68411be81287349
YordiiB <105393316+YordiiB@users.noreply.github.com>
2023-02-10 10:39:02 +0300

    Update ClientDate.java

--- Lab 4/Task1/ClientDate.java
+++ Lab 4/Task1/ClientDate.java
@@ -15,6 +15,7 @@ public class ClientDate {
     public static void main(String args[]) throws Exception
 {
 String s1;
+        
 InterDate h1 = (InterDate)Naming.lookup("DS");
 s1 = h1.display();
 System.out.println(s1);
